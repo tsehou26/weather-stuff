12686dc2eb9e7805d4922563af51ff0ff5287b09
Anthony Huang <tsehou26@berkeley.edu>
2024-03-20 20:04:53 -0700

    Updated index.html on 03/20/2024

---
+++ weather_deploy.py
@@ -0,0 +1,9 @@
+from weatherdata import create_weather
+
+cities = {'Berkeley': 'Yu-Jui', 'Kaohsiung': 'Anthony'}
+
+def main():
+    create_weather(cities)
+
+if __name__ == "__main__":
+    main()
